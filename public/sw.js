db28dff3b796c9a8fce19bfa91f9a8b9fd029068
harun519 <harun519@gmail.com>
2026-09-13 17:04:26 +0300

    Limit Trafo cache cleanup to app caches

--- public/sw.js
+++ public/sw.js
@@ -17,7 +17,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith("trafo-app-") && key !== CACHE_VERSION).map((key) => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
