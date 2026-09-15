3d364841614e64fc17590cceb087a02957b155d4
harun519 <harun519@gmail.com>
2026-09-15 17:20:00 +0300

    Load bundled Leaflet styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 import HaritaEntegrasyon from "./HaritaEntegrasyon";
 import HaritaCikisFix from "./HaritaCikisFix";
 import HaritaNedenFiltresi from "./HaritaNedenFiltresi";
