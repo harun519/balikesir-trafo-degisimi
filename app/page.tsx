45d201cc793fe1a2f13697bb2b9231d38f03322e
harun519 <harun519@gmail.com>
2026-09-20 00:41:43 +0300

    Restore contractor field to removed transformer section

--- app/page.tsx
+++ app/page.tsx
@@ -1864,7 +1864,7 @@ const filtrelenmisKayitlar=useMemo(()=>{
                         <ComboAlani baslik="Trafo Tipi" deger={form.sokulen_trafo_tipi} degistir={v=>formDegistir("sokulen_trafo_tipi",v)} secenekler={TRAFO_TIP_SECENEKLERI} listeId="stip"/>
                         <MetinAlani baslik="Tamir Yılı" deger={form.sokulen_tamir_yili} degistir={v=>formDegistir("sokulen_tamir_yili",v)}/>
                         <MetinAlani baslik="Tamir Firması" deger={form.sokulen_tamir_firmasi} degistir={v=>formDegistir("sokulen_tamir_firmasi",v)}/>
-                      </div>
+                          </div>
                     </section>
 
                     <section className="rounded-2xl border border-emerald-100 bg-white p-5 shadow-[0_10px_32px_rgba(15,23,42,.06)]">
